a56e96d0f1e3415b297a71fba17a72dab3153924
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-11-09 15:20:10 +0100

    Dodanie przykładów z listą

---
+++ Lists/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<string> persons = new List<string>();
+
+            persons.Add("Marek");
+            persons.Add("Inessa");
+            persons.Add("Serhii");
+            persons.Add("Piotr");
+            persons.Add("Szymon");
+            persons.Add("Marek");
+
+            string selectedPerson = persons[2];
+
+            Console.WriteLine(selectedPerson);
+
+            persons[1] = "Łukasz";
+
+            for (int index = 0; index < persons.Count; index++)
+            {
+                string currentPerson = persons[index];
+                Console.WriteLine($"{index} {currentPerson}");
+            }
+
+            // persons.Remove("Marek");
+            persons.RemoveAt(5);
+
+            persons.RemoveAll(MyFilter);
+            persons.RemoveAll(p => p.Contains("Marek"));
+
+          
+
+            persons.Clear();
+
+            foreach (string currentPerson in persons)
+            {
+                Console.WriteLine(currentPerson);
+            }            
+
+        }
+
+        private static bool MyFilter(string p)
+        {
+            return p.Contains("Marek");
+        }
+    }
+}
---
+++ excercises/excercise - basket.md
@@ -0,0 +1,26 @@
+# Zadanie nr 3 - Koszyk zakupowy
+Klient zamówił Sklep internetowy.
+
+Utwórz aplikację konsolową o nazwie **Basket**.
+
+Koszyk zakupowy ma realizować następujące funkcje:
+- dodawanie wybranego produktu do koszyka (np. z katalogu 5 produktów)
+- usuwanie produktu z koszyka na podstawie nazwy produktu
+- wyczyszczenia całego koszyka
+
+Założenia:
+- lista produktów w katalogu może się zmieniać
+
+
+
+
+
+
+
+
+
+
+
+
+
+
