4da6771f6feb5c7a2f6b672d148be0cd5c1fd1d4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-11-22 11:55:28 +0100

    Dodanie przykładu z Html

---
+++ Html/Element.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Html
+{
+    public abstract class Element
+    {
+        public string Id { get; set; }
+
+        public string Style { get; set; }
+        public abstract string Tag { get; }
+
+        public abstract string Render();    // Metoda abstrakcyjna
+
+        //public virtual string Render()
+        //{
+        //    return string.Empty;
+        //}
+    }
+
+    public class Paragraph : Element
+    {
+        public string Content { get; set; }
+
+        public override string Tag
+        {
+            get
+            {
+                return "p";
+            }
+        }
+
+        public override string Render()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("<");
+            stringBuilder.Append(Tag);
+
+            if (!string.IsNullOrEmpty(Id))
+            {
+                stringBuilder.Append($" Id=\"{Id}\"");
+            }
+
+            if (!string.IsNullOrEmpty(Style))
+            {
+                stringBuilder.Append($" style=\"{Style}\">");                
+            }
+
+            // Paragraph
+            stringBuilder.Append(Content);
+
+            // EndTag
+
+            stringBuilder.Append($"</{Tag}>");
+
+            string html = stringBuilder.ToString();
+
+            return html;
+        }
+    }
+
+
+    public class Image : Element
+    {
+        public string Source { get; set; }
+        public string Alternative { get; set; }
+
+        public override string Tag => "img";
+
+        public override string Render()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("<");
+            stringBuilder.Append(Tag);
+
+            if (!string.IsNullOrEmpty(Id))
+            {
+                stringBuilder.Append($" Id=\"{Id}\"");
+            }
+
+            if (!string.IsNullOrEmpty(Style))
+            {
+                stringBuilder.Append($" style=\"{Style}\">");
+            }
+
+
+            // Image
+            if (!string.IsNullOrEmpty(Source))
+            {
+                stringBuilder.Append($" src=\"{Source}\"");
+            }
+
+            if (!string.IsNullOrEmpty(Alternative))
+            {
+                stringBuilder.Append($" alt=\"{Alternative}\"");
+            }
+
+            // EndTag
+
+            stringBuilder.Append($">");
+
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Html/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Html
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Element element = new Paragraph() { Content = "This is some text in a paragraph.", Style = "text-align:right" };
+
+            string result = element.Render();
+
+            string expected = "<p style=\"text-align:right\">This is some text in a paragraph.</p>";
+
+            if (result == expected)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Błąd");
+            }
+
+            Console.WriteLine(result);
+
+            Element image = new Image() { Source = "pic_trulli.jpg", Alternative = "Italian Trulli" };
+
+            string result2 = image.Render();
+            string expected2 = "<img src=\"pic_trulli.jpg\" alt=\"Italian Trulli\">";
+
+            if (result2 == expected2)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Błąd");
+
+                Console.WriteLine(result2);
+                Console.WriteLine(expected2);
+            }
+
+
+
+        }
+    }
+}
