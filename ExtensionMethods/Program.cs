eb8b2d463eb2bee5e0f7d02909a40f0ad9e4e78b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-11-23 11:12:11 +0100

    Problem

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "JOhn";
+            string lastName = "smiTH";
+
+            firstName = firstName.Substring(0, 1).ToUpper() + firstName.Substring(1, firstName.Length - 1).ToLower();
+            lastName = lastName.Substring(0, 1).ToUpper() + lastName.Substring(1, lastName.Length - 1).ToLower();
+
+
+            Console.WriteLine($"{firstName} {lastName}");
+
+
+        }
+    }
+}
