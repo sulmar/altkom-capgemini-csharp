9194b2f985895b116df26e217e4a6755acca1e47
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-11-23 15:09:17 +0100

    Odczyt z pliku i zamiana na obiekty

---
+++ Files/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = "products.csv";
+
+            if (File.Exists(filename))
+            {
+                string content = File.ReadAllText(filename);
+
+              //  Console.WriteLine(content);
+
+                string[] lines = File.ReadAllLines(filename);
+
+                List<Product> products = new List<Product>();
+
+                foreach (var line in lines.Skip(1))
+                {
+                    string[] columns = line.Split(';');
+
+                    string productName = columns[0];
+                    decimal unitPrice = decimal.Parse(columns[1]);
+                    int.TryParse(columns[2], out int discount);
+                    
+                    Product product = new Product
+                    {
+                        Name = productName,
+                        UnitPrice = unitPrice,
+                        Discount = discount
+                    };
+
+                    products.Add(product);
+                    
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Plik {filename} nie istnieje.");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    class Product
+    {
+        // snippet: prop + 2 x Tab
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int? Discount { get; set; }
+    }
+}
--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -9,7 +9,18 @@ namespace ValueAndReferenceTypes
             Console.WriteLine("Typy wartościowe (proste)");
             // int, byte, float, DateTime, bool, struct
 
+            Location location = new Location(52.05, 18.04);
+
+            Console.WriteLine(location);
+            DoWork(ref location);
+            Console.WriteLine(location);
+
             int x = 10;
+
+            DoWork(ref x);
+
+            Console.WriteLine(x);
+
             int y = x;   // <--- kopia x
 
             y++;
@@ -36,6 +47,8 @@ namespace ValueAndReferenceTypes
 
             Person selectedPerson = person;
 
+            DoWork(person);
+
             selectedPerson.FirstName = "Adam";
 
             Console.WriteLine(person.FirstName);
@@ -55,7 +68,7 @@ namespace ValueAndReferenceTypes
                 OrderDate = oldOrder.OrderDate,
                 TotalAmount = 900
             };
-            
+
 
             Order newOrder2 = new Order(oldOrder.Id, oldOrder.OrderDate, oldOrder.TotalAmount, oldOrder.CustomerName)
             {
@@ -72,11 +85,59 @@ namespace ValueAndReferenceTypes
             // Save to Db
 
         }
+
+        public static void DoWork(ref int x)
+        {
+            x++;
+        }
+
+        public static void DoWork(Person person)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            person.FirstName = person.FirstName.ToUpper();
+            Console.WriteLine(person);
+            Console.ResetColor();
+        }
+
+        public static void DoWork(ref Location location)
+        {
+            location.Latitude += 0.1;
+            location.Longitude += 0.2;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(location);
+            Console.ResetColor();
+        }
     }
 
+
+    // Struktura - zbiór pól (typ wartościowy)
+    struct Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public Location(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public override string ToString()
+        {
+            return $"lat: {Latitude} lng: {Longitude}";
+        }
+    }
+
+    // Klasa - typ referencyjny
     class Person
     {
         public string FirstName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName;
+        }
     }
 
     class Order
